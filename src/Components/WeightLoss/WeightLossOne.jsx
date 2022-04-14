e009036ddc812ddf96c75c92295582c1456392d4
lava <lavakumared15@gmail.com>
2022-04-15 00:14:37 +0530

    number check onkeyDown

--- src/Components/WeightLoss/WeightLossOne.jsx
+++ src/Components/WeightLoss/WeightLossOne.jsx
@@ -16,9 +16,9 @@ const WeightLossOne = () => {
             </div>
             <div className='assessment'>
               <h5>Please enter your height and weight below?</h5>
-              <input className='input' type="text" onKeyPress={numberCheck} placeholder='Height(in cms)' />
+              <input className='input' type="text" onkeyDown={numberCheck} placeholder='Height(in cms)' />
               <br />
-              <input className='input' type="text" onKeyPress={numberCheck} placeholder='Weight(in Kgs)' />
+              <input className='input' type="text" onKeyDown={numberCheck} placeholder='Weight(in Kgs)' />
             </div>
         </div>
         <ProceedTemplate text="Proceed" choice={"weightloss-1"} backLink="choice"/>
